a56aa75032d1734e42ceffeeccdddf33cd98737d
Peter Busatti <peterbusatti@gmail.com>
2022-01-23 16:22:11 -0500

    add win and tie checker

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const gameBoard = (() => {
     
     const boardArray = [];
     
-    const init = () => {
+    const render = () => {
         for (let i = 0; i <= 8; i++) {
             const square = document.createElement("div");
                 
@@ -24,20 +24,20 @@ const gameBoard = (() => {
     };
  
     return {
-        init,
+        render,
+        boardArray,
     };
 
 })();
 
-gameBoard.init();
+gameBoard.render();
 
 const Player = (mark) => {
     mark;
 
-
     return {
         mark,
-    }
+    };
 }
 const player1 = Player("X")
 const player2 = Player("O")
@@ -46,31 +46,71 @@ const controller = (() => {
     let currentPlayer = player1;
     const squares = document.querySelectorAll(".square");
     
-    const addMark = (e) => {
+    const takeTurn = (e) => {
         if (e.target.textContent === "") {
-            e.target.textContent = currentPlayer.mark;
-            togglePlayer();
+            e.target.textContent = currentPlayer.mark;            
+            
+            if (isWin(gameBoard.boardArray)) {
+                console.log(`${currentPlayer.mark}'s Win` );
+            }
+            else if (isDraw(gameBoard.boardArray) && !isWin(gameBoard.boardArray)) {
+                console.log("tie");
+            }
+            
+            togglePlayer();  
         } 
-    }
-
-    squares.forEach(square => {
-        square.addEventListener("click", addMark);
-    });
+    };
 
     const clearBoard = () => {
-        squares.forEach(square => {
+        gameBoard.boardArray.forEach(square => {
             square.textContent = "";
         });
-    }
+    };
 
     const togglePlayer = () => {
         if (currentPlayer === player1) {
             currentPlayer = player2;
         }
         else {currentPlayer = player1;}
-    }
+    };
+
+    squares.forEach(square => {
+        square.addEventListener("click", takeTurn);
+    });
+
+    const isWin = (array) => {
+        const mark = currentPlayer.mark
+        const winningArrays = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7 ,8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        return winningArrays.some(combo => {
+            return combo.every(index => {
+                if (array[index].textContent === mark) {
+                    return true;
+                }
+                else {return false}
+            })
+        })
+    };
+
+    const isDraw = (array) => {
+        return array.every(square => {
+            if (square.textContent != "") {
+                return true;
+            }
+            else {return false}
+        })
+    };
 
     return {
-        clearBoard,
-    }
+        clearBoard, 
+    };
 })();
